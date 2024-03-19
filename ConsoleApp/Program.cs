f8e2e3ca4a4a74fe2321ead44b8c53a37db0c427
nhungis <nhung@live.se>
2024-03-19 22:20:20 +0100

    Fun with lists

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,3 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!!!");
+Console.WriteLine("Hello, World!");
+
+List<string> citiesList = new List<string> { "Delhi", "Los Angeles", "Saint Petersburg" };
+
+citiesList.Add("New York City");
+      
+citiesList.Remove("Delhi");
+      
+citiesList.AddRange(new string[] {"Cairo", "Johannesburg"});
+      
+bool hasNewDelhi = citiesList.Contains("New Delhi");
+
+Console.WriteLine(hasNewDelhi);
+
+foreach (var city in citiesList)
+{
+    Console.WriteLine(city);
+}
